949c879a706ecb7c76c3653d7c3933473e2ab450
jerryJiang <jerry@hsbsz8.com>
2022-05-25 15:38:42 +0800

    样式优化

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -9,81 +9,101 @@ function Header() {
     <Disclosure>
       {({ open }) => (
         <>
-          <header class="px-4 md:px-24 shadow">
-            <div class="flex titleBox">
+          <header className="max-w-screen-xl mx-auto my-0">
+            {/* md:px-24 px-4 shadow*/}
+            <div className="flex h-auto py-6 justify-between">
+              {/* justify-around */}
+              {/* titleBox justify-start*/}
               <div>
                 <img src={home_logo} alt="logo" />
               </div>
-              <div class="w-full flex flex-row items-center justify-end space-x-12">
-                <div class="hidden sm:block cursor-pointer">
-                  <NavLink class="notSelected" to="/">
+              <div className="flex flex-row space-x-12 text-xl font-medium items-center">
+                {/* flex flex-row ml-20 space-x-12 text-xl font-medium items-center */}
+                {/* flex flex-row items-center justify-end space-x-12 */}
+                <div className="hidden sm:block cursor-pointer">
+                  <NavLink className="notSelected" to="/">
                     首页
                   </NavLink>
                 </div>
-                <div class="hidden sm:block cursor-pointer">
-                  <NavLink class="notSelected" to="/i">
-                    instagram
+
+                <div className="hidden sm:block cursor-pointer">
+                  <NavLink className="notSelected" to="/i">
+                    Youtube
                   </NavLink>
                 </div>
-                <div class="hidden sm:block cursor-pointer">
-                  <NavLink class="notSelected" to="/s">
-                    youtube
+                <div className="hidden sm:block cursor-pointer">
+                  <NavLink className="notSelected" to="/f">
+                    Facebook
                   </NavLink>
                 </div>
-                <div class="hidden sm:block cursor-pointer">
-                  <NavLink class="notSelected" to="/f">
-                    facebook
+
+                <div className="hidden sm:block cursor-pointer">
+                  <NavLink className="notSelected" to="/i">
+                    Instagram
                   </NavLink>
                 </div>
-                <div class="hidden sm:block cursor-pointer">
-                  <NavLink class="notSelected" to="/t">
-                    twitter
+                <div className="hidden sm:block cursor-pointer">
+                  <NavLink className="notSelected" to="/s">
+                    Shopee
                   </NavLink>
                 </div>
-                <div class="sm:hidden flex items-center">
+                <div className="hidden sm:block cursor-pointer">
+                  <NavLink className="notSelected" to="/t">
+                    Twitter
+                  </NavLink>
+                </div>
+                <div className="hidden sm:block cursor-pointer">
+                  <NavLink className="notSelected" to="/t">
+                    自助下单
+                  </NavLink>
+                </div>
+                <div className="sm:hidden flex items-center">
                   <Disclosure.Button>
                     {open ? (
                       <X
                         title="Close"
-                        class="block h-5 w-5"
+                        className="block h-5 w-5"
                         aria-hidden="true"
                       />
                     ) : (
                       <Burger
                         title="Burger"
-                        class="block h-5 w-5"
+                        className="block h-5 w-5"
                         aria-hidden="true"
                       />
                     )}
                   </Disclosure.Button>
                 </div>
               </div>
+              <div className="flex flex-row items-center">
+                <p>11111</p>
+              </div>
             </div>
           </header>
-          <Disclosure.Panel class="sm:hidden absolute w-full bg-conbg px-4 py-2">
-            <div class="flex flex-col space-y-2">
-              <div class="cursor-pointer">
-                <NavLink class="notSelected" to="/">
+          <Disclosure.Panel className="sm:hidden absolute w-full bg-conbg px-4 py-2">
+            <div className="flex flex-col space-y-2">
+              <div className="cursor-pointer">
+                <NavLink className="notSelected" to="/">
                   首页
                 </NavLink>
               </div>
-              <div class="cursor-pointer">
-                <NavLink class="notSelected" to="/i">
+              <div className="cursor-pointer">
+                <NavLink className="notSelected" to="/i">
                   instagram
                 </NavLink>
               </div>
-              <div class="cursor-pointer">
-                <NavLink class="notSelected" to="/y">
+              <div className="cursor-pointer">
+                <NavLink className="notSelected" to="/y">
                   youtube
                 </NavLink>
               </div>
-              <div class="cursor-pointer">
-                <NavLink class="notSelected" to="/f">
+              <div className="cursor-pointer">
+                <NavLink className="notSelected" to="/f">
                   facebook
                 </NavLink>
               </div>
-              <div class="cursor-pointer">
-                <NavLink class="notSelected" to="/t">
+              <div className="cursor-pointer">
+                <NavLink className="notSelected" to="/t">
                   twitter
                 </NavLink>
               </div>
